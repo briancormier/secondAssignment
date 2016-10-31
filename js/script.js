a15681f5b646e23f107e2a16ae1e067f336ad6d0
briancormier <briancormiers@gmail.com>
2016-10-30 21:31:40 -0400

    initial commit
    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $("#flip").click(function(e){
+    e.preventDefault();
+        $("#panel").slideToggle();
+         $('html, body').animate({
+        scrollTop: 10000
+    }, 2500);
+    });
+});
